6e09f7217ef20963629d7b1be3296797fd6f7900
Augie <augie100801@gmail.com>
2023-08-25 17:08:45 +0700

    FIrst push

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Router, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import Main from './components/Pages/main';
+import AboutMe from "./components/Pages/about-me";
+import Expertise from "./components/Pages/expertise";
+import MyProjects from "./components/Pages/my-projects";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/aboutMe' element={<AboutMe />} />
+        <Route path='/' element={<Main />} />
+        <Route path='/expertise' element={<Expertise />} />
+        <Route path='/myProjects' element={<MyProjects />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
